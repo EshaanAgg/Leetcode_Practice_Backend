8e1a59662da24bfe537a165870f5754f36a12d91
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-05-18 15:43:07 +0530

    Move to deployed database

--- index.js
+++ index.js
@@ -1,48 +1,58 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Company = require("./models/company");
-const Question = require("./models/question");
+const morgan = require("morgan");
+require("dotenv").config();
 
-mongoose.connect("mongodb://localhost:27017/leetcode", { useNewUrlParser: true }).then(() => {
-	const app = express();
+mongoose
+	.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, dbName: "leetcode" })
+	.then(() => {
+		const app = express();
 
-	app.use(express.json());
-	app.use(
-		express.urlencoded({
-			extended: true,
-		})
-	);
+		app.use(express.json());
+		app.use(
+			express.urlencoded({
+				extended: true,
+			})
+		);
+		app.use(morgan("dev"));
 
-	app.listen(5000, () => {
-		console.log("Server has started!");
+		app.listen(5001, () => {
+			console.log("Server has started!");
+		});
+
+		app.get("/company", async (req, res) => {
+			const comapnies = await Company.find().select(["name"]).distinct("name").lean();
+			res.json(comapnies);
+		});
+
+		app.post("/time", async (req, res) => {
+			const { company } = req.body;
+			if (!company) res.json([]);
+			else {
+				const timeline = await Company.find({
+					name: company,
+				})
+					.select("timeline")
+					.lean();
+
+				res.json(timeline.map((obj) => obj.timeline));
+			}
+		});
+
+		app.post("/questions", async (req, res) => {
+			const { company, timeline } = req.body;
+			if (!company || !timeline) res.json([]);
+			else {
+				const questions = await Company.find({
+					name: company,
+					timeline,
+				})
+					.select("questions")
+					.populate("questions")
+					.lean();
+
+				res.json(questions.map((obj) => obj.questions));
+			}
+		});
 	});
-
-	app.get("/company", async (req, res) => {
-		const comapnies = await Company.find().select("name").distinct("name").lean();
-		res.json(comapnies);
-	});
-
-	app.post("/time", async (req, res) => {
-		const { company } = req.body;
-		const timeline = await Company.find({
-			name: company,
-		})
-			.select("timeline")
-			.lean();
-
-		res.json(timeline.map((obj) => obj.timeline));
-	});
-
-	app.post("/questions", async (req, res) => {
-		const { company, timeline } = req.body;
-		const questions = await Company.find({
-			name: company,
-			timeline,
-		})
-			.select("questions")
-			.populate("questions")
-			.lean();
-
-		res.json(questions.map((obj) => obj.questions));
-	});
-});
